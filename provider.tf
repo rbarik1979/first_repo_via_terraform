69c366200dc739e52d0dc92bd181369a8ff4a8f4
rbarik1979 <39639485+rbarik1979@users.noreply.github.com>
2026-07-21 15:09:41 +0530

    terrafrom learning
    provider

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = ""
+  secret_key = ""
+}
+
+
+variable "root_password" {}
